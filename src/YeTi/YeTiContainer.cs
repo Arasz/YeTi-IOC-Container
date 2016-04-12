66ec6a2ab10736476f0e017cfb89b6c3fd919aed
AraszDev <rafal.araszkiewicz@student.put.poznan.pl>
2016-04-12 21:50:58 +0200

    Implement basic IOC functionality
    Metods Register and Resolve were implemented. This metods have only
    basic functionality needed only to pass tests in ideal conditions.

--- src/YeTi/YeTiContainer.cs
+++ src/YeTi/YeTiContainer.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,14 +13,31 @@ namespace YeTi
     /// </summary>
     public class YeTiContainer
     {
+        private readonly Dictionary<Type, Type> _registrations = new Dictionary<Type, Type>();
+
+        /// <summary>
+        /// Registers type to create
+        /// </summary>
+        /// <typeparam name="TRegistration">Registered type</typeparam>
+        /// <typeparam name="TImplementation">Type of object which will be created</typeparam>
         public void Register<TRegistration, TImplementation>() where TImplementation : TRegistration, new()
         {
-            throw new NotImplementedException();
+            _registrations[typeof(TRegistration)] = typeof(TImplementation);
         }
 
-        public T Resolve<T>()
+        /// <summary>
+        /// Resolves given type to registered object
+        /// </summary>
+        /// <typeparam name="T">Type of object which we want to get</typeparam>
+        /// <returns>Object of type <typeparamref name="T"/></returns>
+        public T Resolve<T>() where T : class
         {
-            throw new NotImplementedException();
+            var requestedType = typeof(T);
+            var actualType = _registrations[requestedType];
+
+            var instance = Activator.CreateInstance(actualType) as T;
+
+            return instance;
         }
     }
 }
