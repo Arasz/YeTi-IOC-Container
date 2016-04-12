8a5d4814e2a2c503d62c687e150ec0eca13f230d
AraszDev <rafal.araszkiewicz@student.put.poznan.pl>
2016-04-12 16:34:22 +0200

    Remove xUnit framework, add NUnit framwork and write first unit test which will compile.

---
+++ src/YeTi/YeTiContainer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YeTi
+{
+    /// <summary>
+    /// IOC container
+    /// </summary>
+    public class YeTiContainer
+    {
+        public void Register<TRegistration, TImplementation>() where TImplementation : TRegistration, new()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Resolve<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/YeTi.Tests/YeTiContainerTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YeTi.Tests
+{
+    /// <summary>
+    /// YeTi IOC container test
+    /// <remarks>
+    /// Tests naming convention [UnitOfWork_StateUnderTest_ExpectedBehavior]
+    /// </remarks>
+    /// </summary>
+    [TestFixture]
+    public class YeTiContainerTest
+    {
+        [Test]
+        public void ResolvesRegisteredComponent_RegisteredTypeAsGenericParameter_CreatedObjectOfGivenType()
+        {
+            //Arrange
+            var container = new YeTiContainer();
+            container.Register<ITestInterface, TestInterfaceImplementation>();
+            //Act
+            var resolvedObject = container.Resolve<ITestInterface>();
+            //Assert
+            resolvedObject.ShouldBeOfType<TestInterfaceImplementation>();
+        }
+    }
+
+    public interface ITestInterface
+    {
+    }
+
+    internal class TestInterfaceImplementation : ITestInterface
+    {
+    }
+}
